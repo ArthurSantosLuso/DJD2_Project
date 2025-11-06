b731646841ad21df69b5e427b2717ddc623fae63
unknown <arthurluiz.developer@gmail.com>
2025-11-06 18:43:45 +0000

    Player movement updated.

--- Assets/Scripts/Player/FirstPersonMovement.cs
+++ Assets/Scripts/Player/FirstPersonMovement.cs
@@ -6,23 +6,17 @@ using UnityEngine.Rendering;
 public class FirstPersonMovement : MonoBehaviour
 {
     [Header("Movement Speeds")]
-    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float forwardSpeed = 3.0f;
+    [SerializeField] private float strafeSpeed = 3.0f;
+    [SerializeField] private float backwardSpeed = 3.0f;
 
-    // add sprint?
-
-    [Header("Look Sensitivity")]
+    [Header("Look Sentting")]
     [SerializeField] private float mouseSensitivity = 2.0f;
-    [SerializeField] private float upDownRange = 75.0f;
+    [SerializeField] private float maxLookUpRange= 290.0f;
+    [SerializeField] private float maxLookDownRange = 75.0f;
 
-    [Header("Inputs Customisation")]
-    [SerializeField] private string horizontalMoveInput = "Horizontal";
-    [SerializeField] private string verticalMoveInput = "Vertical";
-
-    [SerializeField] private string MouseXInput = "Mouse X";
-    [SerializeField] private string MouseYInput = "Mouse Y";
-
-    private Camera mainCamera;
-    private float verticalRotation;
+    private Transform head;
+    private Vector3 headRotation;
     private CharacterController characterController;
 
 
@@ -31,34 +25,49 @@ public class FirstPersonMovement : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         characterController = GetComponent<CharacterController>();
-        mainCamera = Camera.main;
+        head = GetComponentInChildren<Camera>().transform;
     }
 
     private void Update()
     {
-        HandleMovement();
         HandleRotation();
+        UpdateHead();    
+    }
+
+    private void HandleRotation()
+    {
+        float mouseXRotation = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseXRotation, 0);
+    }
+
+    private void UpdateHead()
+    {
+        headRotation = head.localEulerAngles;
+
+        headRotation.x -= Input.GetAxis("Mouse Y");
+
+        if (headRotation.x > 180f)
+            headRotation.x = Mathf.Max(maxLookUpRange, headRotation.x);
+        else
+            headRotation.x = Mathf.Min(maxLookDownRange, headRotation.x);
+
+        head.localEulerAngles = headRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
     }
 
     private void HandleMovement()
     {
-        float verticalSpeed = Input.GetAxis(verticalMoveInput) * walkSpeed;
-        float horizontalSpeed = Input.GetAxis(horizontalMoveInput) * walkSpeed;
+        float verticalSpeed = Input.GetAxis("Forward") * forwardSpeed;
+        float horizontalSpeed = Input.GetAxis("Strafe") * forwardSpeed;
 
         Vector3 speed = new Vector3(horizontalSpeed, 0, verticalSpeed);
         speed = transform.rotation * speed;
 
         characterController.SimpleMove(speed);
-    }
-
-    private void HandleRotation()
-    {
-        float mouseXRotation = Input.GetAxis(MouseXInput) * mouseSensitivity;
-        transform.Rotate(0, mouseXRotation, 0);
-
-        verticalRotation -= Input.GetAxis(MouseYInput) * mouseSensitivity;
-        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
-        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
     }
 
 }
