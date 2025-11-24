369a9f6ea5553450ea96d832cd5a1e39c65bb272
unknown <arthurluiz.developer@gmail.com>
2025-11-24 17:39:35 +0000

    Computer password/desktop screen flow working.

--- Assets/Scripts/Computer/ComputerInteraction.cs
+++ Assets/Scripts/Computer/ComputerInteraction.cs
@@ -4,21 +4,43 @@ using UnityEngine;
 public class ComputerInteraction : MonoBehaviour
 {
     [SerializeField] private TMP_InputField inputField;
+    [SerializeField] private GameObject passwordPanel;
+    [SerializeField] private GameObject desktopPanel;
+
+    private bool isPasswordCorrect = false;
+
+    //private void OnEnable()
+    //{
+    //    if (isPasswordCorrect)
+    //    {
+
+    //    }
+    //    else
+    //    {
+    //        desktopPanel.SetActive(false);
+    //        passwordPanel.SetActive(true);
+    //    }
+    //}
+
+    
 
     private void Start()
     {
         inputField.text = "";
-        inputField.onEndEdit.AddListener(TextoFinalizado);
     }
 
-    public void ShowText()
+    public void CheckIfPasswordIsCorrect()
     {
-        string text = inputField.text;
-        Debug.Log(text);
-    }
-
-    void TextoFinalizado(string textoDigitado)
-    {
-        Debug.Log("O jogador terminou de digitar: " + textoDigitado);
+        if (inputField.text == "123456")
+        {
+            Debug.Log("Acertou a password!");
+            desktopPanel.SetActive(true);
+            passwordPanel.SetActive(false);
+            //isPasswordCorrect = true;
+        }
+        else
+        {
+            Debug.Log("Errou a password!");
+        }
     }
 }
